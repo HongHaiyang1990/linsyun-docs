beb4bb8c0ee97bfba50515e0e5ae69a73388f3c6
honghaiyangfulu <honghaiyang@fulu.com>
2022-03-14 13:47:01 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: '灵时云',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  base: '/docs/',
+  description: '操作手册',
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: '引言', link: '/foreword/' },
+      { text: '概述', link: '/summary/' },
+      { text: '使用', link: '/guide/' },
+      { text: '商户端', link: 'http://s.linsyun.com', target: '_blank', rel: 'noopener noreferrer' }
+    ],
+    displayAllHeaders: true,
+    sidebar: 'auto',
+    smoothScroll: true,
+    lastUpdated: '最后更新时间'
+  },
+  plugins: ['@vuepress/back-to-top', '@vuepress/medium-zoom']
+}
