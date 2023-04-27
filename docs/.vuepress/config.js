1ff42f639ea92bf09601cb3c4f13383c0a854ee8
honghaiyangfulu <honghaiyang@fulu.com>
2023-04-27 11:01:56 +0800

    基础

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,45 @@
 module.exports = {
-  title: '灵时云',
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
-  base: '/docs/',
-  description: '操作手册',
-  dest: './dist',
+  title: "灵时云·商户综合平台操作指南",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  base: "/docs/",
+  description: "操作指南",
+  dest: "./dist",
   themeConfig: {
-    logo: '/logo.png',
+    logo: "/logo.png",
     nav: [
-      { text: '引言', link: '/foreword/' },
-      { text: '概述', link: '/summary/' },
-      { text: '使用', link: '/guide/' },
-      { text: '商户端', link: 'http://s.linsyun.com', target: '_blank', rel: 'noopener noreferrer' }
+      { text: "引导", link: "/guide/" },
+      {
+        text: "操作指南",
+        link: "/instruction/",
+        items: [
+          { text: "登录", link: "/instruction/login/" },
+          { text: "任务管理", link: "/instruction/task/" },
+          { text: "创客管理", link: "/instruction/maker/" },
+          { text: "结算管理", link: "/instruction/settlement/" },
+          { text: "我的账户", link: "/instruction/account/" },
+          { text: "我的协议", link: "/instruction/agreement/" },
+          { text: "财务中心", link: "/instruction/finance/" },
+          { text: "票税管理", link: "/instruction/bill/" },
+          { text: "风控管理", link: "/instruction/risk/" },
+          { text: "API对接", link: "/instruction/openapi/" },
+        ],
+      },
+      { text: "API文档", link: "http://linsyun.gitee.io/openapi/", target: "_blank", rel: "noopener noreferrer" },
+      { text: "商户综合平台", link: "http://s.linsyun.com", target: "_blank", rel: "noopener noreferrer" },
     ],
-    displayAllHeaders: true,
-    sidebar: 'auto',
+    displayAllHeaders: false,
+    sidebar: "auto",
+    sidebar: {
+      "/instruction/": [
+        {
+          title: "操作指南",
+          collapsable: false,
+          children: ["login/", "task/", "maker/", "settlement/", "account/", "agreement/", "finance/", "bill/", "risk/", "openapi/"],
+        },
+      ],
+    },
     smoothScroll: true,
-    lastUpdated: '最后更新时间'
+    lastUpdated: "最后更新时间",
   },
-  plugins: ['@vuepress/back-to-top', '@vuepress/medium-zoom']
-}
+  plugins: ["@vuepress/back-to-top", "@vuepress/medium-zoom"],
+};
---
+++ docs/instruction/maker/README.md
@@ -0,0 +1,11 @@
+---
+sidebarDepth: 2
+prev: /instruction/task/
+next: /instruction/settlement/
+---
+
+# 创客管理
+
+点击侧边导航栏-创客列表，查看所有已上传的创客。可以在筛选条件单独输入创客姓名/手机号/证件号查找创客，或根据签约状态、添加时间、签约时间进行筛选搜索。如下图：
+
+![图片7.png](../static/img7.png)
